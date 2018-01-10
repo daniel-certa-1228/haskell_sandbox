644687a811dea616949970c5eaa6f0785dfa2688
daniel-certa-1228 <mfdancerta@gmail.com>
2018-01-10 17:32:01 -0600

    recursion: see recursion

--- sandbox5.hs
+++ sandbox5.hs
@@ -12,3 +12,29 @@ maximum' (x:xs)
 
 --for maximum' [2,5,1]
 -- maximum' [2,5,1] = max 2 (maximum' [5,1] = max 5 (maximum' [1] = 1))
+
+--Replicate
+repExample = replicate 3 5
+--[5,5,5]
+replicate' :: (Num i, Ord i) => i -> a -> [a]
+replicate' n x
+    | n <= 0 = []
+    | otherwise = x:replicate' (n-1) x
+-- replicate' 4 6 == [6,6,6,6]
+
+--Take
+takeExample = take 3 [1,2,3,4,5,6,7]
+--[1,2,3]
+take' :: (Num i, Ord i) => i -> [a] -> [a]
+take' n _
+    | n <= 0 = []
+take' _ []   = []
+take' n (x:xs) = x : take' (n-1) xs
+
+-- take' 3 [4,3,2,1] = 4 : take' (3-1) [3,2,1] : take' (2-1) [2,1] : take' (1-1) [1]
+
+reverse' :: [a] -> [a]
+reverse' [] = []
+reverse' (x:xs) = reverse' xs ++ [x]
+
+--reverse' [5,4,3,2,1] = [1,2,3,4,5]
