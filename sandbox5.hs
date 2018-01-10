2e6e441e85e7ecdae11f34a4298eba50da3ac5ac
daniel-certa-1228 <mfdancerta@gmail.com>
2018-01-10 16:43:00 -0600

    more recursion

--- sandbox5.hs
+++ sandbox5.hs
@@ -9,3 +9,6 @@ maximum' (x:xs)
 --maximum' [1,2,3,4,5] == 5
 --maximum' [13] == 13
 --maximum' [] == "maximum of empty list"
+
+--for maximum' [2,5,1]
+-- maximum' [2,5,1] = max 2 (maximum' [5,1] = max 5 (maximum' [1] = 1))
