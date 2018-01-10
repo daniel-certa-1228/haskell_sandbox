94f05d45378433f4d0044adfdda0c7dbb5c3cadd
daniel-certa-1228 <mfdancerta@gmail.com>
2018-01-10 15:32:17 -0600

    let bindings and stated case expressons

--- sandbox4.hs
+++ sandbox4.hs
@@ -50,3 +50,39 @@ calcBmis :: (RealFloat a) => [(a,a)] -> [a]
 calcBmis xs = [bmi w h | (w, h) <- xs]
     where bmi weight height = weight/height^2
 -- calcBmis [(90,2), (80,1.5)] == [22.5,35.55555555555556]
+
+--'let' bindings
+cylinder :: (RealFloat a) => a -> a -> a
+cylinder r h =
+    let sideArea = 2 * pi * r * h
+        topArea = pi * r ^2
+    in  sideArea + 2 * topArea
+--The form is let <bindings> in <expression>
+-- cylinder 10 10 == 1256.6370614359173
+
+letExample = 4 * (let a=9 in a+1) + 2
+-- 42
+letExample2 = [let square x = x*x in (square 5, square 3, square 2)]
+-- [(25,9,4)]
+letExample3 = (let a=100; b=200; c=300 in a*b*c, let foo="Hey "; bar="there!" in foo ++ bar)
+--(6000000,"Hey there!")
+letExample4  = (let (a,b,c) = (1,2,3) in a+b+c) * 100
+-- 600
+
+calcBmisLet :: (RealFloat a) => [(a,a)] -> [a]
+calcBmisLet xs = [bmi | (w,h) <- xs, let bmi = w/h^2]
+-- calcBmis [(90,2), (80,1.5)] == [22.5,35.55555555555556]
+calcBmisLet2 :: (RealFloat a) => [(a,a)] -> [a]
+calcBmisLet2 xs = [bmi | (w,h) <-xs, let bmi = w/h^2, bmi>=25.0]
+
+--case expressions
+head' :: [a] -> a
+head' [] = error "No head on an empty list, dummy."
+head' (x:_) = x
+--head' [2,5,6,8] == 2
+
+head2 :: [a] -> a
+head2 xs = case xs of [] -> error "No head for empty lists!"  
+                      (x:_) -> x
+--head2 [2,5,6,8] == 2
+
