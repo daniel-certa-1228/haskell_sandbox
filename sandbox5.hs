1828c9161e76270a649486fb9a98175e7b213807
daniel-certa-1228 <mfdancerta@gmail.com>
2018-01-11 15:07:50 -0600

    continuing recursion

--- sandbox5.hs
+++ sandbox5.hs
@@ -13,6 +13,13 @@ maximum' (x:xs)
 --for maximum' [2,5,1]
 -- maximum' [2,5,1] = max 2 (maximum' [5,1] = max 5 (maximum' [1] = 1))
 
+maximum2 :: (Ord a) => [a] -> a
+maximum2 [] = error "There is no maximum of an empty list"
+maximum2 [x] = x
+maximum2 (x:xs) = max x (maximum2 xs)
+-- maximum2 [2,5,7,3] == 7
+-- maximum2 ~ max 2 (maximum2 [5,7,3] ~ max 5 (maximum2 [7,3] ~ max 7 (maximum2 [3])))
+
 --Replicate
 repExample = replicate 3 5
 --[5,5,5]
@@ -21,6 +28,7 @@ replicate' n x
     | n <= 0 = []
     | otherwise = x:replicate' (n-1) x
 -- replicate' 4 6 == [6,6,6,6]
+-- replicate' 4 6 = 6:(replicate' (4-1) 6 = 6:(replicate' (3-1) = 6:(replicate' (2-1) = 6:(replicate' (1-1))
 
 --Take
 takeExample = take 3 [1,2,3,4,5,6,7]
@@ -38,3 +46,5 @@ reverse' [] = []
 reverse' (x:xs) = reverse' xs ++ [x]
 
 --reverse' [5,4,3,2,1] = [1,2,3,4,5]
+--reverse' [3,2,1] ~ reverse' [2,1] ++ 3 ~ reverse' [1] ++ [2,3] ~ reverse [] ++ [1,2,3] == [1,2,3]
+
